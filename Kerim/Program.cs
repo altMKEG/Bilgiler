9203f34c3432a832092bd4d4877a7fa65a539c98
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-21 02:06:40 +0300

    Proje dosyası ekle.

---
+++ Kerim/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string adi;//string ile yazılabilir karakterler atadım//
+string soyadi;
+string ogrencinumarasi;
+string telefonnumarasi;
+
+Console.Write("İsminizi Girin: ");//kullanıcıları kişisel verilerini sordum//
+adi = Console.ReadLine();
+Console.Write("Soyadınızı Girin: ");
+soyadi = Console.ReadLine();
+Console.Write("Öğrenci Numaranızı Girin: ");
+ogrencinumarasi = Console.ReadLine();
+Console.Write("Telefon Numaranızı Girin: ");
+telefonnumarasi = Console.ReadLine();
+
+Console.WriteLine("\nisim: " +  adi + " Soyadı: " + soyadi + " Öğrenci Numarası: " + ogrencinumarasi + " Telefon Numarası: " + telefonnumarasi);//stringleri atayıp + komutu ile istenilenleri yazdım//
+Console.ReadKey();//altta yazan kısmın kalkması için//
